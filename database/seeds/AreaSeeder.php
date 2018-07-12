afc7700308f5bd132b356fdb53733bf03659d52c
Gabrielle Amorim <gabrielleporto19@gmail.com>
2018-07-12 17:21:16 -0300

    Seeder com faker

--- database/seeds/AreaSeeder.php
+++ database/seeds/AreaSeeder.php
@@ -9,10 +9,17 @@ class AreaSeeder extends Seeder
      *
      * @return void
      */
-     public function run()
-     {
-         for($i = 0; $i < 10; $i++){
- 				      DB::table('areas')->insert(['descricao' => 'area'. $i]);
-         }
+     //public function run()
+     //{
+        // for($i = 0; $i < 10; $i++){
+ 		//		      DB::table('areas')->insert(['descricao' => 'area'. $i]);
+         //}
+     //}
+
+     public function run(){
+         DB::table('areas')->insert(['descricao' => 'informatica']);
+         DB::table('areas')->insert(['descricao' => 'licenciatura']);
+         DB::table('areas')->insert(['descricao' => 'engenharia']);
+         DB::table('areas')->insert(['descricao' => 'educacao']);
      }
 }
