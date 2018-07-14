3453c2bf7ca101cfaf5c03d8efe938178bcefb52
Gabrielle Amorim <gabrielleporto19@gmail.com>
2018-07-14 20:24:27 -0300

    Migration tabela atividade

--- app/Evento.php
+++ app/Evento.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Evento extends Model
 {
 	public function area(){
-		return $this->belongsTo('App\Area');    
+		return $this->belongsTo('App\Area');
    }
+
+   public function evento(){
+	   return $this->hasOne('App\Atividade');   
+  }
 }
---
+++ app/Http/Controllers/AtividadeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AtividadeController extends Controller{
+
+    public function listarAtividades(){
+		$atividades = \App\Atividade::all();
+		return view('listarAtividades', ['atividades' => $atividades]);
+    }
+
+}
