f5dc57f41ce042deb89f53298ced227407084570
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2018-07-17 17:51:18 -0300

    "Definição dos frameworks usados, página inicial pronta"

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,18 +11,20 @@
         <title>Pensalight - Gestão de Eventos</title>
 
         <!-- Bootstrap core CSS -->
-        <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
         <!-- Fontes customizadas -->
-        <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
         <!-- Plugin CSS -->
-        <link href="../vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+        <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
 
         <!-- Estilos CSS customizados -->
         <link href="css/freelancer.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+        <link href="css/w3.css" rel="stylesheet">
 
     </head>
 
@@ -32,7 +34,7 @@
         <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
             <div class="container">
                 <a class="navbar-brand js-scroll-trigger" href="/">Pensalight</a>
-                <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler bg-primary navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 Menu
                 <i class="fa fa-bars"></i>
                 </button>
@@ -42,10 +44,10 @@
                             <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/cruds">CRUDS</a>
                         </li>
                         <li class="nav-item mx-0 mx-lg-1">
-                            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
+                            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#slide">Eventos</a>
                         </li>
                         <li class="nav-item mx-0 mx-lg-1">
-                            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
+                            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contato</a>
                         </li>
                     </ul>
                 </div>
@@ -53,17 +55,17 @@
         </nav>
 
         <!-- Header -->
-        <header class="masthead bg-primary text-white text-center">
+        <header class="masthead bg-white text-secondary text-center">
             <div class="container">
-                <img class="img-fluid mb-5 d-block mx-auto" src="images/logoRedondo.png" alt="" style="width:300; height:300px;">
+                <img class="img-fluid mb-5 d-block mx-auto" src="img/logoRedondo.png" alt="" style="width:300; height:300px;">
                 <h1 class="text-uppercase mb-0">Gestão de Eventos</h1>
-                <hr class="star-light">
-                <h2 class="font-weight-light mb-0">Visite os eventos que cercam a nossa faculdade!</h2>
+                <hr class="linha-escura-completa">
+                <h2 class="font-weight-light mb-0">Fique por dentro do que esá acontecendo na faculdade!</h2>
             </div>
         </header>
 
         <!-- Seção de Eventos -->
-        <section class="portfolio" id="portfolio">
+        <!-- <section class="portfolio" id="portfolio">
             <div class="container">
                 <h2 class="text-center text-uppercase text-secondary mb-0">Nossos Eventos</h2>
                 <hr class="star-dark mb-5">
@@ -130,10 +132,128 @@
                     </div>
                 </div>
             </div>
+        </section> -->
+
+        <!-- teste carousel -->
+
+        <section class="portfolio bg-primary text-white mb-0" id="slide">
+            <div class="container">
+                <h2 class="text-center text-uppercase">Próximos eventos...</h2>
+                <hr class="linha-branca-compacta mb-5">
+            </div>
+
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    <!-- <li data-target="#carouselExampleIndicators" data-slide-to="3"></li> -->
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container" id="blocosEventos">
+                            <div class="row">
+
+                                <!-- geracao de bloco no container -->
+
+                                <?php
+                                for ($i=1; $i < 7; $i++) {
+                                ?>
+
+                                <div class="col-md-6 col-lg-4" >
+                                    <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-1">
+                                        <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                                            <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                                                <i class="fa fa-search-plus fa-3x"></i>
+                                            </div>
+                                        </div>
+                                        <img class="img-fluid" src="img/portfolio/cabin.png" alt="">
+                                    </a>
+                                </div>
+
+                                <?php
+                                }
+                                ?>
+
+                            </div>
+                        </div>
+
+                    </div>
+                <div class="carousel-item">
+
+                    <div class="container" id="blocosEventos">
+                        <div class="row">
+
+                            <!-- geracao de bloco no container -->
+
+                            <?php
+                            for ($i=1; $i < 7; $i++) {
+                            ?>
+
+                            <div class="col-md-6 col-lg-4" >
+                                <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-1">
+                                    <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                                        <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                                            <i class="fa fa-search-plus fa-3x"></i>
+                                        </div>
+                                    </div>
+                                    <img class="img-fluid" src="img/portfolio/cake.png" alt="">
+                                </a>
+                            </div>
+
+                            <?php
+                            }
+                            ?>
+
+                        </div>
+                    </div>
+
+                </div>
+                <div class="carousel-item">
+
+                    <div class="container" id="blocosEventos">
+                        <div class="row">
+
+                            <!-- geracao de bloco no container -->
+
+                            <?php
+                            for ($i=1; $i < 7; $i++) {
+                            ?>
+
+                            <div class="col-md-6 col-lg-4" >
+                                <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-1">
+                                    <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                                        <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                                            <i class="fa fa-search-plus fa-3x"></i>
+                                        </div>
+                                    </div>
+                                    <img class="img-fluid" src="img/portfolio/game.png" alt="">
+                                </a>
+                            </div>
+
+                            <?php
+                            }
+                            ?>
+
+                        </div>
+                    </div>
+                </div>
+              </div>
+              </br>
+              </br>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Próximo</span>
+              </a>
+            </div>
         </section>
 
         <!-- Seção Sobre -->
-        <section class="bg-primary text-white mb-0" id="about">
+        <!-- <section class="bg-primary text-white mb-0" id="about">
             <div class="container">
                 <h2 class="text-center text-uppercase text-white">Sobre Nós</h2>
                 <hr class="star-light mb-5">
@@ -152,7 +272,7 @@
                     </a>
                 </div>
             </div>
-        </section>
+        </section> -->
 
         <!-- Seção de Contato -->
         <section id="contact">
@@ -204,7 +324,7 @@
         </section>
 
         <!-- Rodapé -->
-        <footer class="footer text-center">
+        <footer class="footer text-center bg-secondary">
             <div class="container">
                 <div class="row">
                     <div class="col-md-4 mb-5 mb-lg-0">
@@ -255,9 +375,11 @@
             </div>
         </footer>
 
-        <div class="copyright py-4 text-center text-white">
+        <div class="copyright py-4 text-center text-white bg-terciary">
             <div class="container">
-                <small>Copyright &copy; Your Website 2018</small>
+                <small>Copyright &copy; #4t
+                    <a href="https://github.com/elysonromeiro/GestaoEventos">Veja no GitHub!</a>
+                </small>
             </div>
         </div>
 
@@ -283,9 +405,10 @@
                             <hr class="star-dark mb-5">
                             <img class="img-fluid mb-5" src="img/portfolio/cabin.png" alt="">
                             <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                            <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                                <i class="fa fa-close"></i>
-                            Fechar Projeto</a>
+                            <a class="btn btn-primary btn-lg rounded-pill" href="/cruds">
+                                <i class="fa fa-address-card"></i>
+                            Inscrever-se</a>
+
                         </div>
                     </div>
                 </div>
@@ -293,7 +416,7 @@
         </div>
 
         <!-- Portfolio Modal 2 -->
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-2">
+        <!-- <div class="portfolio-modal mfp-hide" id="portfolio-modal-2">
             <div class="portfolio-modal-dialog bg-white">
                 <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
                     <i class="fa fa-3x fa-times"></i>
@@ -312,10 +435,10 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
         <!-- Portfolio Modal 3 -->
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-3">
+        <!-- <div class="portfolio-modal mfp-hide" id="portfolio-modal-3">
             <div class="portfolio-modal-dialog bg-white">
                 <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
                     <i class="fa fa-3x fa-times"></i>
@@ -334,10 +457,10 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
         <!-- Portfolio Modal 4 -->
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-4">
+        <!-- <div class="portfolio-modal mfp-hide" id="portfolio-modal-4">
             <div class="portfolio-modal-dialog bg-white">
                 <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
                     <i class="fa fa-3x fa-times"></i>
@@ -356,11 +479,11 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
 
         <!-- Portfolio Modal 6 -->
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-6">
+        <!-- <div class="portfolio-modal mfp-hide" id="portfolio-modal-6">
             <div class="portfolio-modal-dialog bg-white">
                 <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
                     <i class="fa fa-3x fa-times"></i>
@@ -379,15 +502,15 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
         <!-- Bootstrap core JavaScript -->
-        <script src="../vendor/jquery/jquery.min.js"></script>
-        <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
         <!-- Plugin JavaScript -->
-        <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-        <script src="../vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+        <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
         <!-- Contact Form JavaScript -->
         <script src="js/jqBootstrapValidation.js"></script>
@@ -395,6 +518,7 @@
 
         <!-- Custom scripts for this template -->
         <script src="js/freelancer.min.js"></script>
+        <script src="js/custom.js"></script>
 
     </body>
 
