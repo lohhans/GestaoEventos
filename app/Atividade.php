f21b89d85c433248cc7834d096583fdfd7d47137
Gabrielle Amorim <gabrielleporto19@gmail.com>
2018-08-04 00:13:09 -0300

    Cadastro de Atividade

--- app/Atividade.php
+++ app/Atividade.php
@@ -6,7 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Atividade extends Model{
 
-    protected $fillable = ['descricao', 'valor'];
+    protected $fillable = ['descricao', 'valor', 'evento_id'];
+
+    public static $rules = [
+		'descricao' => 'required',
+		'valor' => 'required',
+		'evento_id' => 'required'
+	];
+
+    public static $messages = [
+		'required'=> 'O campo :attribute é obrigatório'
+	];
 
     public function evento(){
 		return $this->belongsTo('App\Evento');
---
+++ app/Validator/AtividadeValidator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Validator;
+
+use PhpSpec\Laravel\LaravelObjectBehavior;
+use App\Atividade;
+
+class AtividadeValidator{
+
+    public static function validate($dados){
+        $validator = \Validator::make($dados, Atividade::$rules, Atividade::$messages);
+        if(!$validator->errors()->isEmpty()){
+            //throw new ValidationException($validator, "Erro ao validar atividade");
+        }
+    }
+}
