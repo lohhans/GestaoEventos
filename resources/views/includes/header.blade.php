73f599cdf01daf4c3061a4bbb62d7705c3ce6fea
Gabrielle Amorim <gabrielleporto19@gmail.com>
2018-08-04 01:04:50 -0300

    correções

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,6 +3,6 @@
         <img class="img-fluid mb-5 d-block mx-auto" src="img/logoRedondo.png" alt="" style="width:250; height:250px;">
         <h1 class="text-uppercase mb-0">Gestão de Eventos</h1>
         <hr class="linha-escura-completa">
-        <h2 class="font-weight-light mb-0">Fique por dentro do que esá acontecendo na faculdade!</h2>
+        <h2 class="font-weight-light mb-0">Fique por dentro dos próximos eventos!</h2>
     </div>
 </header>
